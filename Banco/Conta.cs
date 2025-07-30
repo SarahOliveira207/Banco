45fc1321ddc1f7b041072a7b1f22876ba1efd08d
ra2457087 <ra2457087@LDI312>
2025-07-30 20:29:58 -0300

    primeiro commit do projeto banco

---
+++ Banco/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public decimal Saldo { get; private set; }
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0;
+        }
+        public void Depositar(decimal valor)
+        {
+            if (valor <= 0)
+                throw new ArgumentException("O valor do depósito deve ser positivo.");
+            Saldo += valor;
+        }
+        public void Sacar(decimal valor)
+        {
+            if (valor <= 0)
+                throw new ArgumentException("O valor do saque deve ser positivo.");
+            if (valor > Saldo)
+                throw new InvalidOperationException("Saldo insuficiente para saque.");
+            Saldo -= valor;
+        }
+
+    }
+}
